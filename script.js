23d758966cbd492181a24748dbc44c601719f493
AlexDolz <aleksejdolznikov3@gmail.com>
2023-08-16 16:50:32 +0200

    updated

--- script.js
+++ script.js
@@ -53,38 +53,38 @@ message.innerHTML =
 // message.style.height =
 //   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
 
-document.documentElement.style.setProperty('--color-primary', 'orangered'); // to make changes in :root in css file from JS
+// document.documentElement.style.setProperty('--color-primary', 'orangered'); // to make changes in :root in css file from JS
 
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company');
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company');
 
-logo.getAttribute('src');
+// logo.getAttribute('src');
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
-logo.classList.add('class');
-logo.classList.remove('class');
-logo.classList.toggle('class');
-logo.classList.contains('class');
+// // Classes
+// logo.classList.add('class');
+// logo.classList.remove('class');
+// logo.classList.toggle('class');
+// logo.classList.contains('class');
 
-// Dont use
-logo.className = 'jonas'; // override all classes
+// // Dont use
+// logo.className = 'jonas'; // override all classes
 
 // **************** Types of events and events handlers *************
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
 // 1 solution
 // h1.addEventListener('mouseenter', () => {
@@ -105,3 +105,32 @@ const h1 = document.querySelector('h1');
 // setTimeout(() => {
 //   h1.removeEventListener('mouseenter', alertH1);
 // }, 300);
+
+// ********************* Bubbling and capturing ********************
+// Example with bubbling and capturing
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', e => {
+  this.StylePropertyMap.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation(); // to disable bubbling, in practice not good idea
+});
+document.querySelector('.nav__links').addEventListener('click', e => {
+  this.StylePropertyMap.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target.e.currentTarget);
+});
+document.querySelector('.nav').addEventListener(
+  'click',
+  e => {
+    this.StylePropertyMap.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true
+);
