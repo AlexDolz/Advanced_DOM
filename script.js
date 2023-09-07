8ccb14bafb9b54e6be45d6d48909a30d8cf49223
AlexDolz <aleksejdolznikov3@gmail.com>
2023-09-07 13:35:12 +0200

    intersection observer API

--- script.js
+++ script.js
@@ -163,3 +163,19 @@ console.log(h1, parentElement.children);
     el.style.transform = 'scale(0.5)';
   }
 });
+
+// *************** Sticky navigation: Intersection Observer API*******
+
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
